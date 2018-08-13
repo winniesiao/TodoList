9d994675a3d590cd3ce8f3a8ba89bd935da97fda
unknown <wuing18@gmail.com>
2018-08-13 23:53:11 +0800

    init commit

---
+++ todolist.js
@@ -0,0 +1,41 @@
+function init() {
+    document.getElementById("defaultOpen").click();
+
+    var app = new Vue({
+        el: '.app',
+        data: {
+            todos: [],
+            newTodo: ''
+        },
+        methods: {
+            addTodo: function(todo) {
+                this.todos.push({
+                        content: todo,
+                        completed: false
+                    })
+                    // console.log(todo);
+                this.newTodo = '';
+            },
+            removeTodo: function(todo) {
+                this.todos.splice(this.todos.indexOf(todo), 1)
+            }
+        }
+    })
+}
+
+
+
+function openList(evt, tabName) {
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" tabActive", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " tabActive";
+}
+window.addEventListener("load", init, false);
